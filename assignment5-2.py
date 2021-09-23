2a09d398bcb6a03c62a078daffe7df53e8157ca5
Hyojun Kim <90163547+hyojun08@users.noreply.github.com>
2021-09-24 07:52:02 +0900

    Add files via upload

---
+++ assignment5-2.py
@@ -0,0 +1,57 @@
+import time
+import random
+import sys
+sys.setrecursionlimit(10000)
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
+
+
+def recbinsearch(L, l, u, target): # 범위, 시작, 끝, 값
+    mid = (l + u) //2
+    if L[mid] == target:
+        return mid
+    elif L[mid] > target:
+        return recbinsearch(L, l, mid-1, target)
+    elif L[mid] < target:
+        return recbinsearch(L, l+1, u, target)
+    else:
+        return -1
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
